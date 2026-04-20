766b758b7ff8d24d430599f58377942fe94384b3
Poongodi Kannaiyan <k.s.poonkodi@gmail.com>
2026-04-20 11:19:30 +0530

    Moved JWT from config to security package and cleaned structure

---
+++ backend/src/main/java/com/example/crud/security/JwtUtil.java
@@ -0,0 +1,61 @@
+package com.example.crud.security;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+	public class JwtUtil {
+
+	    private static final String SECRET_KEY = "mysecretkeymysecretkeymysecretkey"; // min 256-bit
+
+	    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour
+
+	    private Key getSigningKey() {
+	        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+	    }
+
+	    public String generateToken(UserDetails userDetails) {
+
+
+	        return Jwts.builder()
+	                .setSubject(userDetails.getUsername())
+	                .claim("roles", userDetails.getAuthorities())
+	                .setIssuedAt(new Date())
+	                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+	                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+	                .compact();
+	    }
+
+	    public String extractUsername(String token) {
+	        return getClaims(token).getSubject();
+	    }
+
+	    public boolean validateToken(String token, UserDetails userDetails) {
+	        return extractUsername(token).equals(userDetails.getUsername())
+	                && !isTokenExpired(token);
+	    }
+
+	    private boolean isTokenExpired(String token) {
+	        return getClaims(token).getExpiration().before(new Date());
+	    }
+
+	    private Claims getClaims(String token) {
+	        return Jwts.parserBuilder()
+	                .setSigningKey(getSigningKey())
+	                .build()
+	                .parseClaimsJws(token)
+	                .getBody();
+	    }
+	}
+
+
+
